f87b5925a3d072938d144694ba602246512370c7
andy <andy2472625@gmail.com>
2019-03-19 12:48:30 +0800

    feat:
     - add react chartjs 2
     - add react semantic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Header, Image, Modal } from 'semantic-ui-react'
 import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import { HorizontalBar } from 'react-chartjs-2';
 
-class App extends Component {
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+      target: {
+        name: '',
+        index: -1,
+      },
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Modal
+          open={this.state.isModalOpen}
+          onClose={() => this.setState({
+            isModalOpen: false,
+          })}
+        >
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+            <Image wrapped size='medium' src={logo} />
+            <Modal.Description>
+              <Header>{ this.state.target.name }</Header>
+              <p>index: { this.state.target.index }</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+        <HorizontalBar
+          data={data}
+					onElementsClick={(el) => {
+            if (el.length > 0) {
+              const { _index: index = 0 } = el[0];
+              const name = data.labels[index];
+              this.setState({
+                isModalOpen: true,
+                target: {
+                  name,
+                  index,
+                },
+              });
+            }
+					}}
+        />
+      </>
     );
   }
 }
